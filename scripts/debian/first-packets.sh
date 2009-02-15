a81036bbb2d7dc570464423f0739f5777fc15b70
jbbarth <jeanbaptiste.barth@gmail.com>
2009-02-15 14:48:01 +0100

    Updated debian packets (added ri1.8)

--- scripts/debian/first-packets.sh
+++ scripts/debian/first-packets.sh
@@ -3,4 +3,4 @@
 apt-get update
 apt-get upgrade -y
 apt-get install -y wajig screen vim openssh-server \
-                   ruby1.8 ruby1.8-dev irb rake rubygems
+                   ruby1.8 ruby1.8-dev ri1.8 irb rake rubygems
