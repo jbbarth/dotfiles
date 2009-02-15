50490c2cb792056fb8ce5ec9ae96223e17f6c310
jbbarth <jeanbaptiste.barth@gmail.com>
2009-02-15 13:01:06 +0100

    Added a vbox bridge setup script

---
+++ scripts/vbox/create_bridge.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#See: http://samiux.wordpress.com/2008/07/30/bridging-virtualbox-162-on-ubuntu-8041/
+
+if [ "$1" == "" ]; then
+  echo "Usage: $0 <virtual_machine>"
+  exit 1
+fi
+
+echo "auto eth0
+iface eth0 inet manual
+
+auto br0
+iface br0 inet dhcp
+bridge_ports eth0
+bridge_fd 9
+bridge_hello 2
+bridge_maxage 12
+bridge_stp off" >> /etc/network/interfaces
+
+/etc/init.d/networking restart
+
+VBoxAddIF vbox0 `whoami` br0
+VBoxManage modifyvm "$1" -hostifdev1 vbox0
+chmod 0666 /dev/net/tun
+
+echo "Host config ok !"
+echo "Don't forget to attach your host interface to 'vbox0' in your GUI panel"
