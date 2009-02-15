5aed207770776b0b05540cf953811d657f3402ea
jbbarth <jeanbaptiste.barth@gmail.com>
2009-02-15 14:45:53 +0100

    Updated debian packets (added openssh-server)

--- scripts/debian/first-packets.sh
+++ scripts/debian/first-packets.sh
@@ -2,5 +2,5 @@
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y wajig screen vim \
+apt-get install -y wajig screen vim openssh-server \
                    ruby1.8 ruby1.8-dev irb rake rubygems
