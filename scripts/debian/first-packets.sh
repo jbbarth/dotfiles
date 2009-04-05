a7834309e5112dc358dd926fe393a58cca4feec7
jbbarth <jeanbaptiste.barth@gmail.com>
2009-04-05 22:46:24 +0200

    Added libsqlite3-ruby1.8 packet to first-packet.sh

--- scripts/debian/first-packets.sh
+++ scripts/debian/first-packets.sh
@@ -3,6 +3,7 @@
 apt-get update
 apt-get upgrade -y
 apt-get install -y wajig screen vim openssh-server bash-completion \
-                   ruby1.8 ruby1.8-dev ri1.8 irb rake rubygems libopenssl-ruby
+                   ruby1.8 ruby1.8-dev ri1.8 irb rake rubygems \
+                   libopenssl-ruby libsqlite3-ruby1.8
 gem update
 gem install rails ZenTest ruby-debug
