35a7a70d6a05900f1b48aaf53bf80127d02c9453
jbbarth <jeanbaptiste.barth@gmail.com>
2009-04-05 22:40:12 +0200

    Added some rails-related stuff in first-packet.sh

--- scripts/debian/first-packets.sh
+++ scripts/debian/first-packets.sh
@@ -3,4 +3,6 @@
 apt-get update
 apt-get upgrade -y
 apt-get install -y wajig screen vim openssh-server bash-completion \
-                   ruby1.8 ruby1.8-dev ri1.8 irb rake rubygems
+                   ruby1.8 ruby1.8-dev ri1.8 irb rake rubygems libopenssl-ruby
+gem update
+gem install rails ZenTest
