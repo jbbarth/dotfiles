7cb351af54c59200803d4cdf1baeaebe1cb0cc1b
jbbarth <jeanbaptiste.barth@gmail.com>
2009-04-23 22:11:56 +0200

    Do not install rake with a system package but with a gem

--- scripts/debian/first-packets.sh
+++ scripts/debian/first-packets.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get upgrade -y
 apt-get install -y wajig screen vim openssh-server bash-completion \
-                   ruby1.8 ruby1.8-dev ri1.8 irb rake rubygems \
+                   ruby1.8 ruby1.8-dev ri1.8 irb rubygems \
                    libopenssl-ruby libsqlite3-ruby1.8
 gem update
-gem install rails ZenTest ruby-debug
+gem install rails rake ZenTest ruby-debug
