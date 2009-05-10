baa5403126daa78fac9543565853f7c366d822b3
jbbarth <jeanbaptiste.barth@gmail.com>
2009-05-10 02:13:19 +0200

    Added sqlite3 package + useful symlinks to ruby, ri, rdoc

--- scripts/debian/first.sh
+++ scripts/debian/first.sh
@@ -5,17 +5,21 @@ if [ "`whoami`" != "root" ]; then
   exit 1
 fi
 
-###apt-get update
-###apt-get upgrade -y
-###apt-get install -y wajig screen vim openssh-server bash-completion \
-###                   ruby1.8 ruby1.8-dev ri1.8 irb rubygems git-core \
-###                   libopenssl-ruby libsqlite3-ruby1.8
-###gem update
-###gem install rails rake ZenTest ruby-debug
+apt-get update
+apt-get upgrade -y
+apt-get install -y wajig screen vim openssh-server bash-completion \
+                   ruby1.8 ruby1.8-dev ri1.8 irb rubygems git-core \
+                   libopenssl-ruby libsqlite3-ruby1.8 sqlite3
+[ -e /usr/bin/ruby ] || ln -s `which ruby1.8` /usr/bin/ruby
+[ -e /usr/bin/ri ] || ln -s `which ri1.8` /usr/bin/ri
+[ -e /usr/bin/rdoc ] || ln -s `which rdoc1.8` /usr/bin/rdoc
+
+gem update
+gem install rails rake ZenTest ruby-debug
 
 #after that and other things (install/uninstall of insserv)
 #my netbook doesn't restart... is it really safe ? ...
-sed -i 's/^CONCURRENCY=none/CONCURRENCY=shell/' /etc/init.d/rc
+###sed -i 's/^CONCURRENCY=none/CONCURRENCY=shell/' /etc/init.d/rc
 
 #disable multi consoles
 for i in 2 3 4 5 6; do
