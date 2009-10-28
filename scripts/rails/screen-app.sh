4331d5eb26c6768d5e50f77e12f222f9f5e4c1e4
jbbarth <jeanbaptiste.barth@gmail.com>
2009-10-28 17:42:56 +0100

    Removed comments in screen-app.sh

--- scripts/rails/screen-app.sh
+++ scripts/rails/screen-app.sh
@@ -17,27 +17,18 @@ select PROJECT in $(find $DEVEL_DIR -maxdepth 4 -name database.yml | fgrep confi
   
   PS3=$OLD_PS3
   SPROJECT=$(echo $PROJECT | sed 's#/#_#g')
+
   cd $DEVEL_DIR/$PROJECT
+
   screen -d -m $SCREEN_OPTS -S $SPROJECT
   screen -X -S $SPROJECT -p 0 title SERVER
-###  sleep 1
   screen -X -S $SPROJECT -p 0 stuff "ruby script/server -e development --debugger"
   screen -X -S $SPROJECT screen -t CONSOLE 1
-###  sleep 1
   screen -X -S $SPROJECT -p 1 stuff "ruby script/console development"
   screen -X -S $SPROJECT screen -t DEV 2
-###  sleep 1
   screen -X -S $SPROJECT -p 2 stuff "git status"
   #screen -X -S $SPROJECT screen -t AUTOTEST 3
-  #sleep 1
   #screen -X -S $SPROJECT -p 3 stuff "rake db:test:load && autotest 2>&1 |more"
-
-###  sleep 1
-  
-###  firefox http://localhost:3000/
-###  firefox file://$HOME/doc/railsbrain/index.html
-###  firefox file://$HOME/doc/rubybrain/index.html
-
   screen -x $SPROJECT -p 2
   
   break
