ded445a1a1cd33b4f432ae4cbc5d106d7e3fec03
jbbarth <jeanbaptiste.barth@gmail.com>
2009-11-14 13:25:55 +0100

    first.sh: many fixes in first.sh, see: http://blog.sckyzo.com/2009/11/14/mes-tweaks-sur-ubuntu-karmic-9-10/

--- scripts/debian/first.sh
+++ scripts/debian/first.sh
@@ -10,7 +10,8 @@ apt-get upgrade -y
 apt-get install -y wajig screen vim openssh-server bash-completion \
                    ruby1.8 ruby1.8-dev ri1.8 rake irb rubygems1.8 git-core \
                    libopenssl-ruby libsqlite3-ruby1.8 sqlite3 zsh vim make \
-                   lynx subversion sysv-rc-conf wicd sysklogd libxslt
+                   lynx subversion sysv-rc-conf wicd sysklogd libxslt1.1 \
+                   libxslt1-dev
 [ -e /usr/bin/ruby ] || ln -s `which ruby1.8` /usr/bin/ruby
 [ -e /usr/bin/ri ] || ln -s `which ri1.8` /usr/bin/ri
 [ -e /usr/bin/rdoc ] || ln -s `which rdoc1.8` /usr/bin/rdoc
@@ -20,13 +21,18 @@ echo "Desktop machine ? (y/n) "
 read desktop
 if [ "$desktop" == "y" ]; then
   apt-get install conky gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad \
-    msttcorefonts libgsf-bin imagemagick colordiff xchm mplayer mplayerthumbs
+    msttcorefonts ttf-liberation ttf-dejavu libgsf-bin imagemagick colordiff xchm mplayer mplayerthumbs
   #medibuntu (codecs)
   wget http://www.medibuntu.org/sources.list.d/`lsb_release -cs`.list \
     --output-document=/etc/apt/sources.list.d/medibuntu.list && apt-get \
     -q update && sudo apt-get --yes -q --allow-unauthenticated install \
     medibuntu-keyring && apt-get -q update
   apt-get install w32codecs
+  #ifuse for iphone
+  add-apt-repository jonabeck
+  wajig install ifuse
+  echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
+  echo "vm.swapiness=10" >> /etc/sysctl.conf
 fi
 
 echo "Home machine ? (y/n) "
@@ -52,12 +58,11 @@ if [ "$git" == "y" ]; then
 fi
 
 gem update
-gem install rails rake ZenTest ruby-debug wirble hpricot nokigiri webrat \
+gem install rails rake ZenTest ruby-debug wirble hpricot nokogiri webrat \
             rspec-rails rspec cucumber
 
-#after that and other things (install/uninstall of insserv)
-#my netbook doesn't restart... is it really safe ? ...
-###sed -i 's/^CONCURRENCY=none/CONCURRENCY=shell/' /etc/init.d/rc
+#enable concurrency at boot
+sed -i 's/^CONCURRENCY=.*/CONCURRENCY=startpar/' /etc/init.d/rc
 
 #disable multi consoles
 for i in 2 3 4 5 6; do
