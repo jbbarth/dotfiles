ce70787bdab2f014a5978cac6ac31a0d64e624fa
jbbarth <jeanbaptiste.barth@gmail.com>
2012-01-11 18:52:24 +0100

    .zshrc: back to the oldway for detecting if I'm at work ; it's the case only if I have an IP starting with 10.16 or 172.22

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -64,8 +64,8 @@ alias iptables-clear='iptables -P INPUT ACCEPT'
 alias sudo='command sudo '
 alias ssudo='command sudo sh -c '
 function ssh() {
-  #if ip addr show | grep -e "inet 10.16" -e "inet 172.22" >/dev/null; then
-  if ip addr show | grep "eth0" |grep " UP" >/dev/null; then
+  if ip addr show | grep -e "inet 10.167" -e "inet 172.22" >/dev/null; then
+  #if ip addr show | grep "eth0" |grep " UP" >/dev/null; then
     command ssh -F ~/.ssh/config.work $*
   else
     command ssh $*
