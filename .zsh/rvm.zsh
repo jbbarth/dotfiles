e3a27a0353b2cfe20d42cb7c8625dc92d0a89185
jbbarth <jeanbaptiste.barth@gmail.com>
2012-06-20 07:59:29 +0200

    .zshrc: load rvm-related things only if rvm is installed on the machine

--- .zsh/rvm.zsh
+++ .zsh/rvm.zsh
@@ -5,26 +5,31 @@
 [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 [[ -s /usr/local/rvm/scripts/rvm ]] && source /usr/local/rvm/scripts/rvm
 
-#short current rvm env
-current_rvm_env() {
-  res=${GEM_HOME/*\/}
-  res=${res/ruby-}
-  res=$(echo $res|perl -pe 's/-p\d+($|@)/\1/')
-  echo $res
-}
+#continue only if the machine has an RVM setup
+if type rvm >/dev/null 2>&1; then
 
-#prompt with rvm env
-function precmd() {
-  if [[ -z "$SSH_CONNECTION" ]]; then
-    #PROMPT="$(printf '\u263A'):$(current_rvm_env)%# "
-    PROMPT="local:$(current_rvm_env)%# "
-  else
-    PROMPT="%m:$(current_rvm_env)%# "
-  fi
-}
+  #short current rvm env
+  current_rvm_env() {
+    res=${GEM_HOME/*\/}
+    res=${res/ruby-}
+    res=$(echo $res|perl -pe 's/-p\d+($|@)/\1/')
+    echo $res
+  }
 
-# Add RVM to PATH for scripting
-PATH=$PATH:$HOME/.rvm/bin
+  #prompt with rvm env
+  function precmd() {
+    if [[ -z "$SSH_CONNECTION" ]]; then
+      #PROMPT="$(printf '\u263A'):$(current_rvm_env)%# "
+      PROMPT="local:$(current_rvm_env)%# "
+    else
+      PROMPT="%m:$(current_rvm_env)%# "
+    fi
+  }
 
-# Forces to load per project file in screen
-cd .
+  #add RVM to PATH for scripting
+  PATH=$PATH:$HOME/.rvm/bin
+
+  #forces to load per project file in screen
+  cd .
+
+fi
