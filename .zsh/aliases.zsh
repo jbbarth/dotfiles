cd75a5bdda539101218059dd0399c5eb49e323c2
jbbarth <jeanbaptiste.barth@gmail.com>
2012-08-11 23:26:22 +0200

    .zshrc: avoid problems in ssh() on MacOSX where 'ip' command doesn't exist

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -96,7 +96,7 @@ alias iptables-clear='iptables -P INPUT ACCEPT'
 alias sudo='command sudo '
 alias ssudo='command sudo sh -c '
 function ssh() {
-  if ip addr show | grep -e "inet 10.166" -e "inet 10.167" -e "inet 172.22" >/dev/null; then
+  if which ip >/dev/null && ip addr show | grep -e "inet 10.166" -e "inet 10.167" -e "inet 172.22" >/dev/null; then
   #if ip addr show | grep "eth0" |grep " UP" >/dev/null; then
     command ssh -F ~/.ssh/config.work $*
   else
