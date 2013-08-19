6cf276e3d4648b478a4da90674a54a88e9892d31
jbbarth <jeanbaptiste.barth@gmail.com>
2013-08-19 23:35:14 +0200

    .conky: remove .conky and .lua directories, not used anymore

--- .conky/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sleep 5
-if [ -e "/usr/bin/conky" ] && [ ! -z "$XAUTHORITY" ] && ! pgrep conky>/dev/null; then
-  if [ -s "$HOME/.conky/conkyrc.$(hostname)" ]; then
-    conky -c $HOME/.conky/conkyrc.$(hostname) -d -q
-  elif [ -s "$HOME/.conky/conkyrc" ]; then
-    conky -c $HOME/.conky/conkyrc -d -q
-  fi
-fi
