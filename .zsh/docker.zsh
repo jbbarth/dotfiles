b7a62e8df9cfc7130f2eb7fe097ca001e8df3e26
jbbarth <jeanbaptiste.barth@gmail.com>
2013-09-01 09:18:04 +0200

    .zsh: only remove one docker image at a time when calling dkr-remove-test-images

--- .zsh/docker.zsh
+++ .zsh/docker.zsh
@@ -1,3 +1,3 @@
 #aliases for docker
 alias dkr-add-zsh-completion="curl https://raw.github.com/felixr/docker-zsh-completion/master/_docker |sudo tee /usr/share/zsh/functions/Completion/Linux/_docker; source ~/.zshrc"
-alias dkr-remove-test-images="docker images |grep '<none>' |awk '{print \$3}' |xargs --no-run-if-empty echo docker rmi"
+alias dkr-remove-test-images="docker images |grep '<none>' |awk '{print \$3}' |xargs --no-run-if-empty -n 1 echo docker rmi"
