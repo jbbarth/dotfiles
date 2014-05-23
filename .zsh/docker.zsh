a6364288d8727d3cf7b4e302727aba74d3a3a0e8
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-05-23 05:56:30 +0200

    .zsh: refine docker-install and add dockviz binary to installation

--- .zsh/docker.zsh
+++ .zsh/docker.zsh
@@ -2,12 +2,26 @@
 alias docker-add-zsh-completion="curl https://raw.github.com/felixr/docker-zsh-completion/master/_docker |sudo tee /usr/share/zsh/functions/Completion/Linux/_docker; source ~/.zshrc"
 alias docker-remove-test-images="docker images |grep '<none>' |awk '{print \$3}' |xargs --no-run-if-empty -n 1 docker rmi"
 docker-install() {
-  sudo apt-get update
-  sudo apt-get install linux-image-extra-\$(uname -r)
-  curl http://get.docker.io/gpg | sudo apt-key add -
-  echo deb https://get.docker.io/ubuntu docker main |sudo tee /etc/apt/sources.list.d/docker.list
-  sudo apt-get update
-  sudo apt-get install lxc-docker
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "* Installign docker through Homebrew"
+    brew install docker
+    echo "* Installing dockviz (darwin)"
+    curl -L http://gobuild.io/github.com/justone/dockviz/master/darwin/amd64 -o /tmp/dockviz.zip
+    cd ~/bin
+    unzip /tmp/dockviz.zip dockviz
+  else
+    echo "* Installign docker through APT packages"
+    sudo apt-get update
+    sudo apt-get install linux-image-extra-\$(uname -r)
+    curl http://get.docker.io/gpg | sudo apt-key add -
+    echo deb https://get.docker.io/ubuntu docker main |sudo tee /etc/apt/sources.list.d/docker.list
+    sudo apt-get update
+    sudo apt-get install lxc-docker
+    echo "* Installing dockviz (darwin)"
+    curl -L http://gobuild.io/github.com/justone/dockviz/master/linux/amd64 -o /tmp/dockviz.zip
+    cd ~/bin
+    unzip /tmp/dockviz.zip dockviz
+  fi
 }
 alias docker-run="docker run"
 alias docker-irun="docker run -it"
