998292cedcfa92ebad38aa95b0499d1059d534ff
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-06-02 06:24:03 +0200

    .zsh: invert images and containers cleanup in docker-cleanup(), so that old images can be removed after containers using them are removed

--- .zsh/docker.zsh
+++ .zsh/docker.zsh
@@ -1,9 +1,9 @@
 docker-cleanup() {
+  echo "* Removing old containers"
+  docker ps -qa | xargs --no-run-if-empty -n 1 docker rm
   echo "* Removing test (untagged) images"
   docker images |grep '<none>' |awk '{print $3}' | xargs --no-run-if-empty -n 1 docker rmi
   #TODO: replace with: docker rmi $(docker images -q) ? ; not sure it won't delete more images
-  echo "* Removing old containers"
-  docker ps -qa | xargs --no-run-if-empty -n 1 docker rm
 }
 docker-install() {
   if [[ "$OSTYPE" == "darwin"* ]]; then
