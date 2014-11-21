fd7348b86a61d35058c671b61313e11d61fdcfa7
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-11-21 14:38:50 +0100

    .zsh: remove obsolete scp function

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -114,14 +114,6 @@ alias iptables-list='command sudo iptables -nvL --line-numbers'
 alias iptables-clear='iptables -P INPUT ACCEPT'
 alias sudo='command sudo '
 alias ssudo='command sudo sh -c '
-function scp() {
-  if ip addr show | grep -e "inet 10.166" -e "inet 10.167" -e "inet 172.22" >/dev/null; then
-  #if ip addr show | grep "eth0" |grep " UP" >/dev/null; then
-    command scp -F ~/.ssh/config.work $*
-  else
-    command scp $*
-  fi
-}
 if laptop-detect 2>/dev/null; then
   alias mp='mplayer -fs -ao alsa,oss, -idx -af pan=2:0.5:0.5:0.5:0.5'
 else
