aac610d96764f0765e114ae2a8bec20a596adef2
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2015-02-02 10:32:47 +0100

    .zsh: add taillogs alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -134,3 +134,4 @@ loop() { while :; do clear; date; eval $*; sleep 5; done }
 inodes_count() { for d in `sudo gfind -maxdepth 1 -type d |cut -d\/ -f2 |grep -xv . |sort`; do c=$(sudo find $d |wc -l) ; printf "$c $d\n" ; done }
 inodes_top() { inodes_count|sort -nr|head -10 }
 alias rsync='rsync -avz --progress'
+alias taillogs='sudo bash -c "tail -n 0 -F /var/log/syslog /var/log/**/*.log"'
