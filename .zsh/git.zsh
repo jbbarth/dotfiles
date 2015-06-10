bffa5c1cf1b500e3aaf9dca8c232052134fe8377
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2015-06-10 05:18:44 +0200

    .zsh: simplify 'gp'

--- .zsh/git.zsh
+++ .zsh/git.zsh
@@ -2,6 +2,7 @@ export GIT_EDITOR=vi
 alias g='git'
 alias gs='git status -sb'
 alias gd='git diff'
+alias gp='git push'
 gc() {
   todos
   git status --porcelain | grep -e "^M" -e "^A" >/dev/null || ( cd $(git root); git add . )
@@ -24,19 +25,6 @@ todos() {
 }
 wrap200() {
   perl -pe 's/^(.{0,200})(.*)$/$1.($2?"...":"")/e'
-}
-gp() {
-  if grep "remote = origin" .git/config >/dev/null; then
-    git push --all origin
-  elif [ -s .git/FETCH_HEAD ]; then
-    git push --all $(ruby -ne 'puts $_.split.pop' < .git/FETCH_HEAD).git
-  elif test -f .git/config && grep "origin" .git/config >/dev/null; then
-    git push origin master
-  else
-    echo -n "Repo: "
-    read repo
-    git push --all $repo
-  fi
 }
 if which hub >/dev/null; then
   # do not use an alias or shell comp won't work
