176a428c8a31a8bbb11d6cdcb3dff2afb4e979d3
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2015-09-14 15:03:04 +0200

    .zsh: add EC2_HOME

--- .zsh/aws.zsh
+++ .zsh/aws.zsh
@@ -1,3 +1,4 @@
+export EC2_HOME=/usr/local/ec2
 export AWS_AUTO_SCALING_HOME=/usr/local/as-cli/
 test -x /usr/libexec/java_home && export JAVA_HOME=$(/usr/libexec/java_home)
 export PATH=$PATH::/usr/local/as-cli/bin:/usr/local/ec2/bin
